c99b8bcca1859893e0cd6255b8d938b28ec43238
Ekalape <ekalape@outlook.it>
2023-07-16 19:56:01 +0200

    feat: create users and user by id endpoints

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,5 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -13,6 +13,11 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const res = await graphql({
+        schema: schema,
+        source: req.body.query
+      })
+      return res;
       return {};
     },
   });
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,8 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { PrismaClient } from '@prisma/client';
+
+import { GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+const prisma = new PrismaClient();
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +22,40 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: ({
+    Users: {
+      type: new GraphQLList(UserType),
+      args: {},
+      resolve: async (parent, args) => {
+        return prisma.user.findMany()
+      }
+    },
+    User: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve: async (parent, args) => {
+        return prisma.user.findUnique({
+          where: {
+            id: args.id,
+          },
+        })
+      }
+    }
+  })
+})
+export const schema = new GraphQLSchema({
+  query: RootQuery
+})
